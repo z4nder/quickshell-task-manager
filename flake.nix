1a16c6ef4c762f30f5686167323bd68fdb84e350
Gustavo <gusttavo212@gmail.com>
2026-09-02 13:59:24 -0300

    flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "quickshell-task-manager dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" "clippy" "rustfmt" ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rust
+            pkgs.cargo-watch
+            pkgs.yt-dlp
+          ];
+
+          shellHook = ''
+            echo "rust $(rustc --version)"
+            echo "yt-dlp $(yt-dlp --version)"
+          '';
+        };
+      });
+}
